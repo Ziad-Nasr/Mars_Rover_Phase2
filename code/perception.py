fc80380136f8c4c0905c5fcf2e42e4e640827ac9
Abdelrahman Elhawary <abdelrahmanelhawary@Abdelrahmans-MacBook-Pro.local>
2022-12-11 00:08:59 +0200

    implemented basic debuggin mode

--- code/perception.py
+++ code/perception.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 
+# Turn to true for debuggin mode, shows the pipeline on the left of the simulator's screen
+DEBUGING_MODE = True
+
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
 def color_thresh(img, rgb_thresh=(160, 160, 160)):
@@ -75,6 +78,37 @@ def perspect_transform(img, src, dst):
     warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
     return warped
 
+# a function that scales an image down in size by a factor of scale
+def scale_img(img, scale):
+    res = cv2.resize(img, dsize=(int(img.shape[1]/scale), int(img.shape[0]/scale)), interpolation=cv2.INTER_CUBIC)
+    return res
+
+# a function that plots the xpix and ypix of the rover coordinates in an image and shows their mean dir
+def x_y_to_img(xpix, ypix, mean_dir):
+    # create a blank image
+    blank=np.zeros([321,161,3])
+    # loop over the x and y values
+    for idx, x in np.ndenumerate(xpix):
+        # turn the x and y values of rover coordinates to valid image coordinates
+        x = int(160 - ypix[idx])
+        y = int(xpix[idx])
+        # color the x and y in the image white
+        blank[x,y,:] = 255
+    # turn the mean angle to a vector and scale it to be seen
+    x_rov = 50 * np.cos(mean_dir)
+    y_rov = 50 * np.sin(mean_dir)
+    # turn the mean vector from rover coordinates to image coordinates
+    x = int(160 - y_rov)
+    y = int(x_rov)
+    # plot a square cetnered around the mean vector so the direction can be seen visually
+    for i in range(5):
+        for j in range(5):
+            blank[np.clip(x + i, 0, 320), np.clip(y + j, 0,160), :] = [255, 0,0]
+            blank[np.clip(x + i, 0, 320), np.clip(y + j, 0,160), :] = [255, 0,0]
+            blank[np.clip(x - i, 0, 320), np.clip(y + j, 0,160), :] = [255, 0,0]
+            blank[np.clip(x - i, 0, 320), np.clip(y + j, 0,160), :] = [255, 0,0]
+    return blank
+
 # Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
     # Perform perception steps to update Rover()
@@ -104,7 +138,7 @@ def perception_step(Rover):
         # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
         #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
         #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
-    Rover.vision_image[:,:, 2] = threshold_img * 255
+    # moved to the bottom for debugging mode
 
     # 5) Convert map image pixel values to rover-centric coords
     xpix, ypix= rover_coords(threshold_img)
@@ -130,4 +164,29 @@ def perception_step(Rover):
     #if image was intentionally ignored maintain previous angles to keep robot going as it was
     if ignored_img:
         Rover.nav_angles = Rover.prev_angles
+    # if debugging mode is on split the image on the left of the screen to multiple other images
+    if DEBUGING_MODE:
+        # maximum indices in the image on the left
+        max_x = Rover.vision_image.shape[1] 
+        max_y = Rover.vision_image.shape[0]
+        # the points where we split the images both horizontally and vertically
+        half_x = int(max_x/2)
+        half_y = int(max_y-80)
+
+        # show the Warped Image on the top left
+        Rover.vision_image[0:half_y,0:half_x, 2] = scale_img(warped[:,:,2], 2)
+        Rover.vision_image[0:half_y,0:half_x, 1] = scale_img(warped[:,:,1], 2)
+        Rover.vision_image[0:half_y,0:half_x, 0] = scale_img(warped[:,:,0], 2)
+
+        # show thes Threshholded image on the bottom left
+        Rover.vision_image[half_y:max_y,0:half_x, 2] = scale_img(threshold_img * 255, 2)
+
+        # show Rover Coordinates Image and the mean angle on the rightss
+        rover_img = scale_img(x_y_to_img(xpix, ypix, np.mean(Rover.nav_angles)), 2)
+        Rover.vision_image[:,half_x:half_x+80, 2] = rover_img[:,:,2]
+        Rover.vision_image[:,half_x:half_x+80, 1] = rover_img[:,:,1] 
+        Rover.vision_image[:,half_x:half_x+80, 0] = rover_img[:,:,0] 
+    else:
+        Rover.vision_image[:,:, 2] = threshold_img * 255
+
     return Rover
